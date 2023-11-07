b0e86775da043148677275c7999adba74b57032a
jrodriguezc160 <jrcortes02102005@educarex.es>
2023-11-07 12:36:18 +0100

    hover y focus

--- src/components/CrudForm.js
+++ src/components/CrudForm.js
@@ -53,7 +53,7 @@ export const CrudForm = ({
 
   return (
     <div>
-      <h3>Agregar</h3>
+      <h3>{dataToEdit ? 'Editar' : 'Agregar'}</h3>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
