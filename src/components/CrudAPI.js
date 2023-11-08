a38414619329ab787abb04cbe714583134e4d40b
jrodriguezc160 <jrcortes02102005@educarex.es>
2023-11-08 09:50:37 +0100

    curso react 23. api falsa

--- src/components/CrudAPI.js
+++ src/components/CrudAPI.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { CrudForm } from './CrudForm';
+import { CrudTable } from './CrudTable';
 
 export const CrudAPI = () => {
+  const [db, setDb] = useState([]);
+  const [dataToEdit, setDataToEdit] = useState(null);
+
+  const createData = (data) => {
+    data.id = Date.now();
+    console.log(data);
+    setDb([...db, data]);
+  };
+
+  const updateData = (data) => {
+    let newData = db.map((el) => (el.id === data.id ? data : el));
+    setDb(newData);
+  };
+
+  const deleteData = (id) => {
+    let isDelete = window.confirm(
+      '¿Estás seguro de eliminar el registro con el id "' + id + '"?'
+    );
+
+    if (isDelete) {
+      let newData = db.filter((el) => el.id !== id);
+      setDb(newData);
+    } else {
+      return;
+    }
+  };
+
   return (
     <>
-    <h2>Crud API</h2>
+      <h3>CRUD APP</h3>
+      <br />
+      <div className="contenedor-principal">
+        <div className="contenedor-iz">
+          <CrudForm
+            createData={createData}
+            updateData={updateData}
+            dataToEdit={dataToEdit}
+            setDataToEdit={setDataToEdit}
+          />
+        </div>
+        <div className="contenedor-de">
+          <CrudTable
+            data={db}
+            deleteData={deleteData}
+            setDataToEdit={setDataToEdit}
+          />
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
