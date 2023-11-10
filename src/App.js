f8b82a436bfae0b548047a08e132382874529361
jrodriguezc160 <jrcortes02102005@educarex.es>
2023-11-10 09:24:17 +0100

    dark mode v1

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { CrudApp } from './components/CrudApp';
 import NavBar from './components/NavBar';
 import CrudAPI from './components/CrudAPI';
 import SongSearch from './components/SongSearch';
+import DarkTheme from './temaOscuro';
 
 function App() {
   return (
     <>
       <NavBar />
+      <DarkTheme btn=".dark-theme-btn" classDark="dark-mode" />
       <div className="content">
         <h1>Ejercicios con React</h1>
         <hr />
