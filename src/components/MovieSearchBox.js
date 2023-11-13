7a8c5974c7ca99b34e4e5e542bf3ca1decef8d9e
jrodriguezc160 <jrcortes02102005@educarex.es>
2023-11-13 14:12:04 +0100

    input con icono de busqueda

---
+++ src/components/MovieSearchBox.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MovieSearchBox = (props) => {
+  return (
+    <div>
+      <label>
+        <input placeholder="ej. Avengers" className='input-con-icono'></input>
+      </label>
+    </div>
+  );
+};
+
+export default MovieSearchBox;
--- src/components/MoviesApp.js
+++ src/components/MoviesApp.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import MoviesList from './MoviesList';
-import MovieListHeading from './MoviesListHeading';
+import MovieListHeading from './MovieListHeading';
+import MovieSearchBox from './MovieSearchBox';
 
 const MoviesApp = () => {
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState('');
 
   const getMovieRequest = async () => {
-    const url = 'http://www.omdbapi.com/?s={}&apikey=c003a867';
+    const url = `http://www.omdbapi.com/?s=avengers&apikey=c003a867`;
 
     const response = await fetch(url);
 
@@ -25,7 +26,8 @@ const MoviesApp = () => {
   return (
     <>
       <div className="content">
-        <MovieListHeading heading='MOVIES'/>
+        <MovieListHeading heading="MOVIES" />
+        <MovieSearchBox />
       </div>
 
       <div>
