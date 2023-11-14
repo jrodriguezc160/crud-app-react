4ff50af7b79d190a6daecaa8c7a5e0fc5da439b1
jrodriguezc160 <jrcortes02102005@educarex.es>
2023-11-14 09:33:34 +0100

    amo el css

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import { IconoCorazon } from './Iconos';
 
 const MoviesList = (props) => {
   return (
     <>
-    <div className="movies-container">
-      <div className="margen-manual">&nbsp;</div>
-      {props.movies.map((movie, index) => (
-        <div className="movie-item">
-          <div class="ambilight">
-            <img src={movie.Poster} alt="Image1" className="image" />
-            <img src={movie.Poster} alt="" className="light" />
+      <div className="movies-container">
+        <div className="margen-manual">&nbsp;</div>
+        {props.movies.map((movie, index) => (
+          <div className="movie-item" key={index}>
+            <div className="ambilight">
+              <div className="me-gusta" onClick={IconoCorazon}>
+                <IconoCorazon ancho="16px" alto="16px" />
+              </div>
+              <img src={movie.Poster} alt="Image1" className="image" />
+              <img src={movie.Poster} alt="" className="light" />
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
     </>
   );
 };
