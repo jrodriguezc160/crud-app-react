34d1fd359d6f44d385245f3a05346ef09b0fc058
jrodriguezc160 <jrcortes02102005@educarex.es>
2023-11-15 10:15:41 +0100

    control de css + icono ampliar pero no sale el modal

--- src/components/Iconos.js
+++ src/components/Iconos.js
@@ -138,7 +138,6 @@ export function IconoCorazon(props) {
     setTimeout(() => {
       setActivo(false);
     }, 500);
-
   }
 
   return (
@@ -160,32 +159,45 @@ export function IconoCorazon(props) {
   );
 }
 
-export function IconoCorazonFavoritos(props) {
-  const [activo, setActivo] = useState(false);
-
-  function animacionCorazon() {
-    setActivo(true);
-
-    setTimeout(() => {
-      setActivo(false);
-    }, 500);
-  }
-
+export function IconoInfo(props) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width={props.ancho}
       height={props.alto}
       viewBox="0 0 24 24"
-      fill="whitesmoke"
+      fill="none"
       stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className={`feather feather-heart ${activo ? 'active' : ''}`}
-      onClick={animacionCorazon}
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-info"
     >
-      <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" />
+      <circle cx="12" cy="12" r="10" />
+      <line x1="12" y1="16" x2="12" y2="12" />
+      <line x1="12" y1="8" x2="12.01" y2="8" />
+    </svg>
+  );
+}
+
+export function IconoMax(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={props.ancho}
+      height={props.alto}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-maximize-2"
+    >
+      <polyline points="15 3 21 3 21 9" />
+      <polyline points="9 21 3 21 3 15" />
+      <line x1="21" y1="3" x2="14" y2="10" />
+      <line x1="3" y1="21" x2="10" y2="14" />
     </svg>
   );
 }
--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,7 +1,29 @@
 import React from 'react';
-import { IconoCorazon, IconoCorazonFavoritos } from './Iconos';
+import { IconoCorazon, IconoMax } from './Iconos';
 
 const MoviesList = (props) => {
+  const movieInfo = (movie) => {
+    console.log('CLICK');
+    return (
+      <div className="screen">
+        <div className="modal">
+          <div className="modal-img">
+            <img src={movie.Poster} alt="Displayed first" className="image" />
+            <img src={movie.Poster} alt="Ambilight effect" className="light" />
+          </div>
+          <div className="modal-info">
+            <h2>{movie.title}</h2>
+            <h4>{movie.director}</h4>
+            <p>
+              {movie.year} · {movie.actors} · IMDB id: {movie.imdbID}
+            </p>
+            <p>{movie.synopsis}</p>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <>
       <div
@@ -39,19 +61,31 @@ const MoviesList = (props) => {
             />
           ) : (
             props.movies.map((movie, index) => (
-              <div className="movie-item" key={index}>
+              <div
+                className="movie-item"
+                key={index}
+                onClick={() => movieInfo(movie)}
+              >
                 <div className="ambilight">
-                  <div
-                    className="me-gusta"
-                    onClick={() => props.handleFavouritesClick(movie)}
-                  >
-                    <IconoCorazon
-                      ancho="16px"
-                      alto="16px"
-                      esFavorito={props.favourites.some(
-                        (favourite) => favourite.imdbID === movie.imdbID
-                      )}
-                    />
+                  <div className="iconos">
+                    <div
+                      className="me-gusta"
+                      onClick={() => props.handleFavouritesClick(movie)}
+                    >
+                      <IconoCorazon
+                        ancho="16px"
+                        alto="16px"
+                        esFavorito={props.favourites.some(
+                          (favourite) => favourite.imdbID === movie.imdbID
+                        )}
+                      />
+                    </div>
+                    <div
+                      className="ver-info"
+                      onClick={() => movieInfo(movie)}
+                    >
+                      <IconoMax ancho="16px" alto="16px" />
+                    </div>
                   </div>
                   <img
                     src={movie.Poster}
