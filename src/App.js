125da6a4c7a6980cf5f4faee992630f26ddb7975
jrodriguezc160 <jrcortes02102005@educarex.es>
2023-11-28 09:34:09 +0100

    first deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import { CrudApp } from './components/CrudApp';
-import NavBar from './components/NavBar';
-import CrudAPI from './components/CrudAPI';
 import DarkTheme from './temaOscuro';
 import MoviesApp from './components/MoviesApp';
 
@@ -9,27 +6,11 @@ function App() {
   return (
     <>
       <DarkTheme btn=".dark-theme-btn" classDark="dark-mode" />
-      <div className="content-x-nomargin" style={{marginTop: "4vh"}}>
+      <div className="content-x-nomargin" style={{ marginTop: '4vh' }}>
         <h1>Ejercicios con React</h1>
         <hr />
       </div>
       <MoviesApp />
-      <div className="content">
-        <br />
-        <br />
-        <hr />
-        <br />
-        <br />
-        <CrudAPI />
-        <br />
-        <br />
-        <hr />
-        <br />
-        <br />
-        <CrudApp />
-        <br />
-        <br />
-      </div>
     </>
   );
 }
